8c7c77e0652ff084bd5aba6f08274eacba417e3e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-05-02 11:50:29 +0300

    Spritesheet generation

--- src/bin/data-process/main.rs
+++ src/bin/data-process/main.rs
@@ -1,9 +1,9 @@
 use factorio_web_calculator::data::*;
 
-use std::{collections::HashMap, iter::Iterator, path::{PathBuf, Path}, fs::File, fmt::format};
+use std::{collections::{HashMap, HashSet}, iter::Iterator, path::{PathBuf, Path}, fs::File};
 use serde_json::{Value, from_reader, to_writer, from_value, json};
 use clap::Parser;
-use image::{RgbaImage, ImageBuffer, io::Reader, Rgba, Pixel, GenericImageView, imageops::overlay, GenericImage, ImageFormat};
+use image::{RgbaImage, ImageBuffer, io::Reader, Rgba, Pixel, GenericImageView, imageops::overlay, ImageFormat};
 
 #[derive(Debug, Parser)]
 #[clap(about, long_about = None)]
@@ -30,7 +30,7 @@ struct PathResolver {
 
 impl PathResolver {
     fn new(core_path: PathBuf, base_path: PathBuf, out_dir: &Path) -> Self {
-        Self{core_path, base_path, gen_path: out_dir.join("generated-icons/")}
+        Self{core_path, base_path, gen_path: out_dir.join("generated/generated-icons/")}
     }
 
     fn resolve(&self, name: &str) -> PathBuf {
@@ -44,7 +44,46 @@ impl PathResolver {
     }
 }
 
+struct SpriteSheet {
+    sheet: RgbaImage,
+    size: usize,
+    pos: (usize, usize)
+}
+
+impl SpriteSheet {
+    fn new(len: usize) -> Self {
+        let mut i = 1;
+        while (i * i) < len {
+            i += 1
+        }
+        let image = RgbaImage::new((i * 64) as u32, (i * 64) as u32);
+        Self{sheet: image, size: i * 64, pos: (0, 0)}
+    }
+
+    fn add_sprite(&mut self, image: RgbaImage) -> (usize, usize) {
+        overlay(&mut self.sheet, &image, self.pos.0 as i64, self.pos.1 as i64);
+        self.advance()
+    }
+
+    fn advance(&mut self) -> (usize, usize) {
+        let old_pos = self.pos;
+        if self.pos.0 + 64 >= self.size {
+            self.pos.0 = 0;
+            self.pos.1 += 64
+        } else {
+            self.pos.0 += 64
+        }
+        old_pos
+    }
+
+    fn write(&self, path: impl AsRef<Path>) {
+        let mut file = File::create(path).unwrap();
+        self.sheet.write_to(&mut file, ImageFormat::Png).unwrap();
+    }
+}
+
 fn main() {
+    // Init //
     let params = CliParameters::parse();
 
     let difficulty = params.expensive.then(|| "expensive").unwrap_or("normal");
@@ -60,20 +99,27 @@ fn main() {
 
     let path_resolver = PathResolver::new(core_path, base_path, &out_dir);
 
+    // Json data parse //
+
     println!("Parsing input data");
     let in_file = File::open(params.input_file).unwrap();
     let json_data: Value = from_reader(in_file).unwrap();
 
     let game_data = get_data(difficulty, &json_data);
-    println!("Done parsing data");
+    println!("Done parsing data, writing to {}", out_file_path.to_str().unwrap());
 
-    let mut icon_map: HashMap<String, Vec<String>> = HashMap::new();
-    let mut complex_icons: HashMap<String, Vec<IconData>> = HashMap::new();
+    let out_file = File::create(out_file_path).unwrap();
+    to_writer(out_file, &game_data).unwrap();
+
+    // Icons //
+
+    let mut simple_icons: HashMap<String, HashSet<String>> = HashMap::new();    // map path to vec of items that use this icon
+    let mut complex_icons: HashMap<String, Vec<IconData>> = HashMap::new(); // map name of item to icon data
 
     println!("Processing icons for items");
     game_data.items.iter().for_each(|(_, item)| {
         match &item.icon {
-            Icon::Simple(icon) => icon_map.entry(icon.clone()).or_insert(vec![]).push(format!("item-{}", &item.name)),
+            Icon::Simple(icon) => {simple_icons.entry(icon.clone()).or_insert_with(HashSet::new).insert(format!("item-{}", &item.name));},
             Icon::Icons(icons) => {complex_icons.insert(format!("item-{}", &item.name), icons.clone());},
         };
     });
@@ -81,36 +127,57 @@ fn main() {
     println!("Processing icons for assembling machines");
     game_data.assembling_machines.iter().for_each(|(_, item)| {
         match &item.icon {
-            Icon::Simple(icon) => icon_map.entry(icon.clone()).or_insert(vec![]).push(format!("assembling-machine-{}", &item.name)),
+            Icon::Simple(icon) => {simple_icons.entry(icon.clone()).or_insert_with(HashSet::new).insert(format!("assembling-machine-{}", &item.name));},
             Icon::Icons(icons) => {complex_icons.insert(format!("assembling-machine-{}", &item.name), icons.clone());},
         }
     });
 
+    println!("Processing complex icons");
     let complex_icons: HashMap<String, RgbaImage> = complex_icons.into_iter().map(|(k, icons)| generate_complex_icon(k, icons, &path_resolver)).collect();
 
+    println!("Writing complex icons");
     for (name, icon_image) in &complex_icons {
-        let path = path_resolver.resolve(name);
-        println!("{}", path.to_str().unwrap());
+        let mut path = path_resolver.resolve(name);
+        path.set_extension("png");
         let mut file = File::create(path).unwrap();
         icon_image.write_to(&mut file, ImageFormat::Png).unwrap();
     }
 
-    let out_file = File::create(out_file_path).unwrap();
-    to_writer(out_file, &game_data).unwrap();
+    let mut simple_icons: HashMap<RgbaImage, HashSet<String>> = simple_icons.into_iter().map(|(path, items)| {
+        println!("{}", path);
+        let image = Reader::open(path_resolver.resolve(&path))
+            .unwrap()
+            .decode()
+            .unwrap()
+            .to_rgba8()
+            .view(0, 0, 64, 64)
+            .to_image();
+        (image, items)
+    }).collect();
+
+    simple_icons
+        .extend(complex_icons
+            .into_iter()
+            .map(|(k, v)| (v, [k].into())));
+
+    let mut spritesheet = SpriteSheet::new(simple_icons.len());
+
+    let icons: HashMap<(usize, usize), HashSet<String>> = simple_icons.into_iter().map(|(image, name)| {
+        (spritesheet.add_sprite(image), name)
+    }).collect();
+
+    spritesheet.write(out_dir.join("generated/spritesheet.png"));
 }
 
 fn generate_complex_icon(name: String, icons: Vec<IconData>, resolver: &PathResolver) -> (String, RgbaImage) {
     let mut result = ImageBuffer::from_pixel(64, 64, [0, 0, 0, 0].into());
     for icon_data in icons {
-        println!("{:?}", icon_data);
         let icon_path = resolver.resolve(&icon_data.icon);
-        println!("{}", icon_path.to_str().unwrap());
         let mut icon_image = Reader::open(icon_path)
             .unwrap()
             .decode()
             .unwrap()
-            .as_rgba8()
-            .unwrap()
+            .to_rgba8()
             .view(0, 0, 64, 64)
             .to_image();
         icon_image.pixels_mut().map(|p| tint_pixel(p, &icon_data.tint)).for_each(drop);
@@ -118,7 +185,7 @@ fn generate_complex_icon(name: String, icons: Vec<IconData>, resolver: &PathReso
         
     }
 
-    (format!("{}.png", name), result)
+    (name, result)
 }
 
 fn tint_pixel(pixel: &mut Rgba<u8>, tint: &TintColor) {
