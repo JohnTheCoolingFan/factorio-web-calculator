74eb717ae4630cae0dc215b7e96878d895391af6
JohnTheCoolingFan <43478602+JohnTheCoolingFan@users.noreply.github.com>
2022-05-06 09:29:17 +0000

    Deploying to gh-pages from @ JohnTheCoolingFan/factorio-web-calculator@b40a754b15016fc837e154192f90a445ce404a39 🚀

--- factorio-web-calculator-8906f1bfe860176.js
+++ factorio-web-calculator-8906f1bfe860176.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('factorio-web-calculator-71e227b8080b7cbb_bg.wasm', import.meta.url);
+        input = new URL('factorio-web-calculator-8906f1bfe860176_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
