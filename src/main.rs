3ef86b7228436078ddd44817b491edbcb44bc825
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-07-07 03:43:47 +0300

    Fetching icon mapping

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod icon_map;
 use components::*;
 use factorio_web_calculator::data::GameData;
 use gloo_net::http::Request;
+use icon_map::IconMap;
 use once_cell::sync::Lazy;
 use std::{rc::Rc, sync::RwLock};
 use yew::prelude::*;
@@ -36,11 +37,13 @@ fn switch(route: Route) -> Html {
 #[derive(Debug)]
 pub struct AppRoot {
     game_data: Option<Rc<GameData>>,
+    icon_map: Option<Rc<IconMap>>,
 }
 
 #[derive(Debug)]
 pub enum AppRootMessage {
     GameDataReady(Rc<GameData>),
+    IconMapREady(Rc<IconMap>),
 }
 
 impl AppRoot {
@@ -62,6 +65,25 @@ impl AppRoot {
             },
         }
     }
+
+    async fn fetch_icon_map() -> AppRootMessage {
+        match Request::get("/factorio-web-calculator/assets/generated/spritesheet-mapping.json")
+            .send()
+            .await
+        {
+            Err(req_err) => {
+                log::error!("Failed to request spritesheet icon mapping: {}", req_err);
+                panic!("Failed to request spritesheet icon mapping: {}", req_err);
+            }
+            Ok(response) => match response.json().await {
+                Err(parse_err) => {
+                    log::error!("Failed to parse spritesheet mapping: {}", parse_err);
+                    panic!("Failed to parse spritesheet mapping: {}", parse_err);
+                }
+                Ok(icon_map) => AppRootMessage::IconMapREady(Rc::new(icon_map)),
+            },
+        }
+    }
 }
 
 impl Component for AppRoot {
@@ -72,8 +94,20 @@ impl Component for AppRoot {
     fn create(ctx: &Context<Self>) -> Self {
         let scope = ctx.link();
         scope.send_future(Self::fetch_game_data());
+        scope.send_future(Self::fetch_icon_map());
 
-        Self { game_data: None }
+        Self {
+            game_data: None,
+            icon_map: None,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            AppRootMessage::GameDataReady(game_data) => self.game_data = Some(game_data),
+            AppRootMessage::IconMapREady(icon_map) => self.icon_map = Some(icon_map),
+        }
+        true
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
