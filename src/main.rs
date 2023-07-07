6cbe2905d69c1ce7a760045e010abed5799badd9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-07-07 04:04:24 +0300

    Providing user settings and resetting data

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use factorio_web_calculator::data::GameData;
 use gloo_net::http::Request;
 use icon_map::IconMap;
 use once_cell::sync::Lazy;
-use std::{rc::Rc, sync::RwLock};
+use std::{ops::Deref, rc::Rc, sync::RwLock};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -16,6 +16,28 @@ pub static USER_SETTINGS: Lazy<RwLock<UserSettings>> = Lazy::new(|| {
     let result = UserSettings::create();
     RwLock::new(result)
 });
+
+#[derive(Debug)]
+pub struct WrappedUserSettings {
+    user_settings: RwLock<UserSettings>,
+}
+
+impl PartialEq for WrappedUserSettings {
+    fn eq(&self, other: &Self) -> bool {
+        let lock = self.user_settings.read().unwrap();
+        let other_lock = other.user_settings.read().unwrap();
+
+        lock.eq(&other_lock)
+    }
+}
+
+impl Deref for WrappedUserSettings {
+    type Target = RwLock<UserSettings>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.user_settings
+    }
+}
 
 // todo: move game data to AppRoot and do the same with user settings and icon mapping
 
@@ -34,16 +56,19 @@ fn switch(route: Route) -> Html {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct AppRoot {
     game_data: Option<Rc<GameData>>,
     icon_map: Option<Rc<IconMap>>,
+    user_settings: Option<Rc<WrappedUserSettings>>,
 }
 
 #[derive(Debug)]
 pub enum AppRootMessage {
+    ResetData,
     GameDataReady(Rc<GameData>),
     IconMapReady(Rc<IconMap>),
+    UserSettingsReady(Rc<WrappedUserSettings>),
 }
 
 impl AppRoot {
@@ -84,6 +109,10 @@ impl AppRoot {
             },
         }
     }
+
+    async fn init_user_settings() -> AppRootMessage {
+        todo!()
+    }
 }
 
 impl Component for AppRoot {
@@ -93,19 +122,28 @@ impl Component for AppRoot {
 
     fn create(ctx: &Context<Self>) -> Self {
         let scope = ctx.link();
-        scope.send_future(Self::fetch_game_data());
-        scope.send_future(Self::fetch_icon_map());
+        scope.send_message(AppRootMessage::ResetData);
 
-        Self {
-            game_data: None,
-            icon_map: None,
-        }
+        Self::default()
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        let scope = ctx.link();
         match msg {
-            AppRootMessage::GameDataReady(game_data) => self.game_data = Some(game_data),
+            AppRootMessage::ResetData => {
+                *self = Self::default();
+
+                scope.send_future(Self::fetch_game_data());
+                scope.send_future(Self::fetch_icon_map());
+            }
+            AppRootMessage::GameDataReady(game_data) => {
+                self.game_data = Some(Rc::clone(&game_data));
+                scope.send_future(Self::init_user_settings());
+            }
             AppRootMessage::IconMapReady(icon_map) => self.icon_map = Some(icon_map),
+            AppRootMessage::UserSettingsReady(user_settings) => {
+                self.user_settings = Some(user_settings)
+            }
         }
         true
     }
@@ -113,13 +151,16 @@ impl Component for AppRoot {
     fn view(&self, _ctx: &Context<Self>) -> Html {
         let game_data_context = self.game_data.clone();
         let icon_map_context = self.icon_map.clone();
+        let user_settings_context = self.user_settings.clone();
 
         html! {
             <ContextProvider<Option<Rc<GameData>>> context = {game_data_context}>
             <ContextProvider<Option<Rc<IconMap>>> context = {icon_map_context}>
+            <ContextProvider<Option<Rc<WrappedUserSettings>>> context = {user_settings_context}>
                 <BrowserRouter>
                     <Switch<Route> render={switch} />
                 </BrowserRouter>
+            </ContextProvider<Option<Rc<WrappedUserSettings>>>>
             </ContextProvider<Option<Rc<IconMap>>>>
             </ContextProvider<Option<Rc<GameData>>>>
         }
